41092f640535c0485a1054dcf16497e0ee7993dc
Payton Bissell <junk@thebissells.com>
2014-07-08 20:59:28 -0700

    Initial Code

---
+++ www/home.php
@@ -0,0 +1,20 @@
+<?php
+
+//---------------------------------------------------------------------------------------------
+// 
+//---------------------------------------------------------------------------------------------
+$page="home";
+$pagetitle = "Athena";
+
+include("includes/common.php");
+include("includes/top.php");
+showTop(1);
+?>
+ 
+<h2>Athena</h2>
+
+<?php
+showBottom();
+include("includes/bottom.php");
+?>
+
