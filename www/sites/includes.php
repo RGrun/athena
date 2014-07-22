36700f8d83dd2ff04263e15a42b31f16160e06c6
Richard Grunert <r.grun@live.com>
2014-07-22 16:56:01 -0700

    Still trying to figure out structure of dev server

--- www/sites/includes.php
+++ www/sites/includes.php
@@ -2,9 +2,9 @@
 
 	//includes.php
 	
-	require_once $_SERVER['DOCUMENT_ROOT'] . "/athena/www/utils/htmlUtils.php";
-	require_once $_SERVER['DOCUMENT_ROOT'] . "/athena/www/utils/dbConnector.php";
-	require_once $_SERVER['DOCUMENT_ROOT'] . "/athena/www/utils/dbWorker.php";
+	require_once "/athena/www/utils/htmlUtils.php";
+	require_once "/athena/www/utils/dbConnector.php";
+	require_once "/athena/www/utils/dbWorker.php";
 
 	
 ?>
