19e12f8167cd5d5713ff365a0b8c6387017cd454
Richard Grunert <r.grun@live.com>
2014-07-23 16:06:32 -0700

    This should fix the include problem.

--- www/index.php
+++ www/index.php
@@ -2,7 +2,8 @@
 
 	//index.php
 	
-	require_once $_SERVER['DOCUMENT_ROOT'] . "/athenalocal/utils/htmlUtils.php";
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/athena/www/utils/htmlUtils.php";
+
 	
 	$htmlUtils = new htmlUtils();
 	
