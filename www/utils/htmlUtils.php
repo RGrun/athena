79789aa47db076bad2a4b9042bb22f334404ff6f
Richard Grunert <r.grun@live.com>
2014-07-25 15:51:47 -0700

    its kind of like playing whack-a-mole with all these login bugs

--- www/utils/htmlUtils.php
+++ www/utils/htmlUtils.php
@@ -61,7 +61,8 @@ _END;
 				echo "Current User: $userStr <br/>";
 				echo "<a href='/athena/www/landing.php'>Return to landing.</a><br/><br/>";
 			} else {
-				echo "<p>Please log in to Athena. <br/> <br/> <a href='login.php'>Click Here</a> to log in.</p>";
+				die("<p>Please log in to Athena. <br/> <br/> <a href='login.php'>Click Here</a> to log in.</p>");
+				
 			}
 		}
 		
