c5788f5ec6260e43b2ea3074f76373a682e4ba85
Richard Grunert <r.grun@live.com>
2014-07-25 15:54:10 -0700

    Forgot a quotation mark

--- www/utils/htmlUtils.php
+++ www/utils/htmlUtils.php
@@ -62,7 +62,7 @@ _END;
 				echo "<a href='/athena/www/landing.php'>Return to landing.</a><br/><br/>";
 			} else {
 				//die("<p>Please log in to Athena. <br/> <br/> <a href='login.php'>Click Here</a> to log in.</p>");
-				header("Location: /athena/www/login.php);
+				header("Location: /athena/www/login.php");
 				
 			}
 		}
