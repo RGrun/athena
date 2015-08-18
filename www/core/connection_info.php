89664e66126a4366ce3012a2089000821d910d3e
Richard G. <r.grun@live.com>
2015-08-18 01:07:39 -0700

    Update connection_info.php

--- www/core/connection_info.php
+++ www/core/connection_info.php
@@ -6,8 +6,8 @@
 		
 		//THESE NEED CHANGING ONCE PRODUCTION SERVER IS LIVE
 		const HOST = "localhost"; 
-		const USER = "root";
-		const PW = "abcd1234";
+		const USER = "xxx";
+		const PW = "xxxxx";
 		const DBNAME = "athena";
 		
 		public function doConnect();
